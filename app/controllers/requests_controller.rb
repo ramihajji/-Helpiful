abbae298b2ea1e17f1c26a049bf8f22586ff4ae3
Alexandra Hedman <hedman.alexandra@gmail.com>
2022-05-25 11:20:36 +0200

    fixed error with search bar

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,54 +2,58 @@ class RequestsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
   before_action :set_request, only: [ :show, :update, :destroy, :edit]
 
-    def new
-      @request = Request.new
-      authorize @request
-    end
-
-    def create
-      @request = Request.new
-      @request.user = current_user
-      authorize @request
-      if @request.save
-        redirect_to request_path(@request)
-      else 
-        render :new
-      end
-    end
-
-    def index
+  def index
+    @requests = policy_scope(Request)
+    if params[:search][:category].empty? && params[:search][:city].empty?
       @requests = policy_scope(Request)
+    else
+      @requests = Request.where(category: params[:search][:category], address: params[:search][:city])
     end
+  end
 
-    def show
+  def new
+    @request = Request.new
+    authorize @request
+  end
+
+  def create
+    @request = Request.new
+    @request.user = current_user
+    authorize @request
+    if @request.save
+      redirect_to request_path(@request)
+    else
+      render :new
     end
+  end
 
-    def update
-      if @request.update(request_params)
-        redirect_to @request
-      else
-        render :edit
-      end
-    end 
+  def show
+  end
 
-    def destroy
-      @request.delete
-      redirect_to requests
+  def update
+    if @request.update(request_params)
+      redirect_to @request
+    else
+      render :edit
     end
+  end
 
-    def edit
-    end
+  def destroy
+    @request.delete
+    redirect_to requests
+  end
 
-    private 
+  def edit
+  end
 
-    def request_params
-      params.require(:request).permit(:title, :description, :address, :price, :status)
-    end 
+  private
 
-    def set_request
-      @request = Request.find(params[:id])
-      authorize @request
-    end
+  def request_params
+    params.require(:request).permit(:title, :description, :address, :price, :status)
+  end
 
+  def set_request
+    @request = Request.find(params[:id])
+    authorize @request
+  end
 end
