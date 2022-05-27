e77faed30865c6864842310c0a91e34f554ba186
Alexandra Hedman <hedman.alexandra@gmail.com>
2022-05-27 13:58:30 +0200

    change profile info completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :requests
   has_many :offers, through: :requests
-  has_one_attached :image
+  has_one_attached :avatar
 end
